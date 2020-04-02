2073f4e9186fcac2bb76ba97c8ffe17c014e7bd2
“shawnb0002” <shawnb0002@yahoo.com>
2020-04-01 22:46:05 -0400

    Updated the contact for so that it can take use input

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardTitle } from "reactstrap";
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
  function RenderCampsite({campsite}) {
@@ -8,7 +8,6 @@ import { Card, CardImg, CardText, CardTitle } from "reactstrap";
     <div className="col-md-5 m-1">
       <Card>
         <CardImg top src={campsite.image} alt={campsite.name} />
-        <CardTitle>{campsite.name}</CardTitle>
         <CardText>{campsite.description}</CardText>
       </Card>
     </div>
@@ -44,8 +43,18 @@ import { Card, CardImg, CardText, CardTitle } from "reactstrap";
       return (
         <div className="container">
           <div className="row">
+              <div className="col">
+                  <Breadcrumb>
+                      <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                      <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                  </Breadcrumb>
+                  <h2>{props.campsite.name}</h2>
+                  <hr />
+              </div>
+          </div>
+          <div className="row">
             <RenderCampsite campsite= {props.campsite} />
-            <RenderComments comments={props.campsite.comments} />
+            <RenderComments comments={props.comments} />
           </div>
         </div>
 
